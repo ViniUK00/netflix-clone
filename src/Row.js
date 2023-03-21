7c5318c5b4a883210893db77cc2b5bef8496f1c2
ViniUK00 <ervin.bilo@yahoo.com>
2023-03-21 23:13:23 +0000

    Get all the movies and builds rows

---
+++ src/Row.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import instance from './axios';
+import './Row.css'
+
+const base_url = "http://image.tmdb.org/t/p/original/"
+
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        // if [], run once when the row loads and dont run again
+        async function fetchData() {
+            const request = await instance.get(fetchUrl)
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+        // Whenever i use anything inside the useEffect if there is any variable that has been pulled of outside
+        // and used it inside the useEffect have to include it to the square brackets (as a 2nd argument)
+    },[fetchUrl]) 
+
+    console.log(movies);
+  return (
+    <div className='row'>
+        <h2>{title}</h2>
+        
+        <div className='row__posters'>
+          {movies?.map(movie => (
+            <img
+            key={movie.id}
+            className='row_poster'
+            src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          ))}
+        </div>
+        {/* container -> posters */}
+    </div>
+  )
+}
+
+export default Row
