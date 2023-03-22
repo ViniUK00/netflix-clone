f8ab510c3181ca57520b6d9ac6f1ded8336bd6c7
ViniUK00 <ervin.bilo@yahoo.com>
2023-03-22 02:12:10 +0000

    finishing the project

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
+import movieTrailer from 'movie-trailer';
 import React, { useState, useEffect } from 'react'
+import YouTube from 'react-youtube';
 import instance from './axios';
 import './Row.css'
 
@@ -6,6 +8,9 @@ const base_url = "http://image.tmdb.org/t/p/original/"
 
 function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
+
+    const [trailerUrl, setTrailerUrl] = useState("");
+
 
     // A snippet of code which runs based on a specific condition/variable
     useEffect(() => {
@@ -20,7 +25,27 @@ function Row({title, fetchUrl, isLargeRow}) {
         // and used it inside the useEffect have to include it to the square brackets (as a 2nd argument)
     },[fetchUrl]) 
 
-    console.log(movies);
+    const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    const handleClick = (movie)=> {
+      if (trailerUrl) {
+        setTrailerUrl('');
+      } else {
+        movieTrailer(movie?.name || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch(error => console.log(error))
+      }
+    }
+
   return (
     <div className='row'>
         <h2>{title}</h2>
@@ -29,11 +54,12 @@ function Row({title, fetchUrl, isLargeRow}) {
           {movies?.map(movie => (
             <img
             key={movie.id}
+            onClick={()=> handleClick(movie)}
             className={`row_poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`} alt={movie.name} />
           ))}
         </div>
-        {/* container -> posters */}
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
